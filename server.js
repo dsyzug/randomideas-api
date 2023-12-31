d3b2f6a54aabbed5bd1a4f5e78ee2c13c2732a1d
Dhirendra Yadav <ds.yadav.iitd@gmail.com>
2024-01-01 00:53:32 +0530

    dotenv and mongoose config done

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require('express');
-const port = 5000;
+require('dotenv').config(); // load .env file
+const port = process.env.PORT || 5000;
+const connectDB = require('./config/db');
+
+connectDB();
+
 const app = express();
 
 // body parser middleware
